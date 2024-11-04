93bb5c739ffbf532b7af7417ff2d897668d424ae
hazycora <git@hazy.gay>
2024-11-03 21:09:55 -0600

    Update login page, refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,20 @@
-<h1>hazel's loops</h1>
-
-<form method="post">
-	<label>
-		Email
-		<input type="text" name="email" placeholder="jane@example.com" />
-	</label>
-	<label>
-		Password
-		<input type="password" name="password" placeholder="****" />
-	</label>
-	<input type="submit" value="Log in" />
-</form>
-
-<footer>
-	<a href="https://git.gay/h/loops-web">source code</a>
-</footer>
+<div class="page">
+	<form method="post">
+		<p>An unofficial client for <a href="https://loops.videos">Loops</a></p>
+		<label>
+			Email
+			<input type="text" name="email" placeholder="jane@example.com" />
+		</label>
+		<label>
+			Password
+			<input type="password" name="password" placeholder="****" />
+		</label>
+		<input type="submit" value="Log in" />
+		<p>
+			<a href="https://git.gay/h/loops-web">source code</a>
+		</p>
+	</form>
+</div>
 
 <style lang="postcss">
 	form {
@@ -30,10 +30,5 @@
 			display: grid;
 			grid-template-columns: 8rem 1fr;
 		}
-	}
-
-	footer {
-		margin-block-start: 1rem;
-		border-block-start: 1px solid rgb(255 255 255 / 0.2);
 	}
 </style>
