98ea5e9421f5c4bceb8c855f24c3cd83a861a5c0
hazycora <git@hazy.gay>
2024-11-24 03:39:36 -0600

    switch to new /api/v0/user/username endpoint
    thanks dansup

--- src/routes/(app)/@[username]/+layout.server.ts
+++ src/routes/(app)/@[username]/+layout.server.ts
@@ -2,9 +2,7 @@ import type { Account, FollowState } from '$lib/types'
 
 async function getAccount(username: string, options: { fetch: typeof fetch }) {
 	const { fetch } = options
-	const userResponse = await fetch(
-		`/api/hazel/user/byUsername/${username}?ext=1`
-	)
+	const userResponse = await fetch(`/api/v0/user/username/${username}?ext=1`)
 	return <{ data: Account; meta: FollowState }>await userResponse.json()
 }
 
--- src/routes/api/hazel/user/search/+server.ts
+++ src/routes/api/hazel/user/search/+server.ts
@@ -19,7 +19,7 @@ async function getByUsername(
 	options: { fetch: typeof fetch }
 ) {
 	const { fetch } = options
-	const response = await fetch(`/api/hazel/user/byUsername/${username}`)
+	const response = await fetch(`/api/v0/user/username/${username}`)
 	if (!response.ok) {
 		return null
 	}
